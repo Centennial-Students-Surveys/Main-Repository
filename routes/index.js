bd2e7890a62636eae8504a0b3f9e1c59b9266ecb
Bohdan Drozd <drozd.bogdan35@gmail.com>
2022-11-09 19:00:08 -0500

    "First initial commit"

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',{title: 'Home Page' });
+});
+
+router.get('/studfeeds', function(req, res, next)
+{
+  res.render('studfeeds', {title: 'Students feedbacks page' });
+});
+
+router.get('/profrate', function(req, res, next)
+{
+  res.render('profrate', {title: 'Professors rate page' });
+});
+
+router.get('/about', function(req, res, next)
+{
+  res.render('about', {title: 'About college page' });
+});
+
+router.get('/sign_in', function(req, res, next)
+{
+  res.render('sign_in', {title: 'Sign in page' });
+});
+
+module.exports = router;
