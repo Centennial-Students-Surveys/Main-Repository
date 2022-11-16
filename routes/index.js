af51a76179d196e1e32df044c15617dae1f05571
Bohdan Drozd <drozd.bogdan35@gmail.com>
2022-11-16 12:22:10 -0500

    "minor fixes 0.2"

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,46 @@ router.get('/', function(req, res, next) {
   res.render('index',{title: 'Home Page' });
 });
 
+//Route to Student's feedbacks page
 router.get('/studfeeds', function(req, res, next)
 {
   res.render('studfeeds', {title: 'Students feedbacks page' });
 });
 
+//Route to Professor's rate page
 router.get('/profrate', function(req, res, next)
 {
   res.render('profrate', {title: 'Professors rate page' });
 });
 
+//Route to About College info
 router.get('/about', function(req, res, next)
 {
   res.render('about', {title: 'About college page' });
 });
 
+//Route to sigh in page (if authorization was successfull) <needs to add a user id from db to /accout#id(post)
 router.get('/sign_in', function(req, res, next)
 {
   res.render('sign_in', {title: 'Sign in page' });
 });
 
+//Route to user's settings from accout(only for authorized users)
 router.get('/settings', function(req, res, next)
 {
   res.render('settings/settings', {title: 'Settings' });
+});
+
+//Route to user's account
+router.get('/account', function(req,res, next)
+{
+  res.render('account/account', {title: 'Hello user <Name>'});
+});
+
+//Route to sign up form(only for students)
+router.get('/signup',function(req,res,next)
+{
+  res.render('signup/signup', {title: 'Registration Page'});
 });
 
 module.exports = router;
