4bc35798df0e677c7b5191344d145a95802e2e04
Maksat Dushanov <66558579+MaksatDushanov@users.noreply.github.com>
2022-11-23 11:51:08 -0500

    created modules

---
+++ models/anonymous.js
@@ -0,0 +1,46 @@
+// require modules for the Anonymous Model
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
+let Anonymous = mongoose.Schema({
+    feedback: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Feedback is required'
+    },
+    stars: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: 'email address is required'
+    },
+    complextiveLevel: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Complextive Level is required'
+    },
+    department: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Department is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    update: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    collection: "anonymouss"
+});
+
+// configure options for Anonymous Model
+
+let options = ({ missingPasswordError: 'Wrong / Missing Password' });
+Anonymous.plugin(passportLocalMongoose, options);
+module.exports.Anonymous = mongoose.model('Anonymous', Anonymous);
---
+++ models/student.js
@@ -0,0 +1,64 @@
+// require modules for the Student Model
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
+let Student = mongoose.Schema({
+    firstName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'First Name is required'
+    },
+    lastName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Last Name is required'
+    },
+    email: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Email is required'
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'password is required'
+    },
+    stars: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Stars is required'
+    },
+    feedback: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Feedback is required'
+    },
+    department: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Department is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    update: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    collection: "students"
+});
+
+// configure options for Students Model
+
+let options = ({ missingPasswordError: 'Wrong / Missing Password' });
+Student.plugin(passportLocalMongoose, options);
+module.exports.Student = mongoose.model('Student', Student);
