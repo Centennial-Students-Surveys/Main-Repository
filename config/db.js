2b9c3e89a53c26bc68ffa9e8880ca601e852c672
Maksat Dushanov <66558579+MaksatDushanov@users.noreply.github.com>
2022-11-27 18:31:51 -0500

    fixed DB config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "URI": "mongodb+srv://user:wlrHdsJr9d5obbVP@cluster1.7rdpjwx.mongodb.net/?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://user:wlrHdsJr9d5obbVP@cluster1.7rdpjwx.mongodb.net/centennialSurveys?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
